64a1970de1847dff0cfdc892f8c4b37649a5e67e
MaxSavchuck <avatar2945@gmail.com>
2021-09-08 17:01:18 +0300

    add buttons and some corrections 2

--- src/components/ThemeSwitcher/index.jsx
+++ src/components/ThemeSwitcher/index.jsx
@@ -3,13 +3,24 @@ import Switch from 'react-switch';
 import styles from './ThemeSwitcher.module.scss';
 
 function ThemeSwitcher () {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(true);
   const handleChange = () => {
     setChecked(!checked);
   };
   return (
     <div className={styles.themeSwitcher}>
-      <Switch checked={checked} onChange={handleChange} />
+      <Switch
+        checked={checked}
+        onChange={handleChange}
+        onColor='#c5f6fa'
+        // onHandleColor='#2693e6'
+        handleDiameter={30}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        boxShadow='0px 1px 5px rgba(0, 0, 0, 0.6)'
+        height={30}
+        width={58}
+      />
     </div>
   );
 }
